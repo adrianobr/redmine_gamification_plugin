e057f8e0002b70f25a66a04674ee8d2d2793b078
Marco Castro <marcodotcastro@gmail.com>
2016-05-20 19:13:08 -0300

    Atividade #76 Adicionar Show

--- app/controllers/gamification_premia_controller.rb
+++ app/controllers/gamification_premia_controller.rb
@@ -1,32 +1,33 @@
-class GamificationPremiumController < ApplicationController
+class GamificationPremiaController < ApplicationController
   #unloadable
 
-  before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :new,  :show, :edit, :update, :destroy]
+  before_action :set_premium, only: [:show, :edit, :update, :destroy]
 
   def index
-    @premiums = GamificationPremium.all
+    @premia = GamificationPremium.all
   end
 
   def show
   end
 
   def new
-    @user = User.new
+    @premium = GamificationPremium.new
   end
 
   def edit
   end
 
   def create
-    @user = User.new(user_params)
+    @premium = GamificationPremium.new(premium_params)
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+      if @premium.save
+        format.html { redirect_to @premium, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @premium }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @premium.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,19 +57,16 @@ class GamificationPremiumController < ApplicationController
 
   private
     def set_user
-      if params[:id]
-        user_id = params[:id]
-      else
-        user_id = User.current.id
-      end
+      user_id = User.current.id
       @user = Gamification.find_by_user_id(user_id)
-      if !@user
-        @user = Gamification.find_by_user_id(User.current.id)
-      end
     end
 
-    def user_params
-      params.require(:user).permit(:name)
+    def set_premium
+      @premium = GamificationPremium.find(params[:id])
+    end
+
+    def premium_params
+      params.require(:gamification_premium).permit(:description, :tickets)
     end
 
 end
