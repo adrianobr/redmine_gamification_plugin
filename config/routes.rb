b652e16d194f5ff104f55d87cc32230cd61a7a55
Marco Castro <marcodotcastro@gmail.com>
2016-05-20 14:20:45 -0300

    Atividade #70 Criar Rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 get 'gamification/tutorial' => 'gamification#tutorial'
-get 'gamification/premium' => 'gamification#premium'
+
 post 'gamification/regist_rating' => 'gamification#regist_rating'
 put 'gamification/upload_image' => 'gamification#upload_image'
 get 'gamification/remove_image' => 'gamification#remove_image'
@@ -13,7 +13,6 @@ get 'gamification/rating' => 'gamification#rating'
 get 'gamification/badges' => 'gamification#badges'
 get 'projects/:project_id/gamification' => 'gamification#project'
 get 'gamification/project' => 'gamification#project'
-get 'gamification/premium_create' => 'gamification#premium_create'
 get 'gamification/create' => 'gamification#create'
 get 'gamification/destroy' => 'gamification#destroy'
 get 'gamification/error' => 'gamification#error'
@@ -21,3 +20,6 @@ get 'gamification/entry' => 'gamification#entry'
 get 'gamification/total' => 'gamification#total'
 get 'gamification', :to => 'gamification#index'
 get 'gamification/:id', :to => 'gamification#index'
+
+get 'gamification_premium/premium' => 'gamification_premium#premium'
+get 'gamification_premium' => 'gamification_premium#index'
